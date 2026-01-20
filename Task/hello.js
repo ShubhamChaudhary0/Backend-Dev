67a63875ea543cf47041837c67559ce0b8ec7656
ShubhamChaudhary0 <chaudharyshubham1947@gmail.com>
2026-01-20 14:55:28 +0530

    done

--- Task/hello.js
+++
@@ -1,4 +0,0 @@
-//console.log("hello,world");
-const {calculator}=require ('./hello1');
-console.log(calculator(5,10,'+'));
-console.log(calculator(10,5,'-'));
--- Task/practice.js
+++
@@ -1,7 +0,0 @@
-const greet=()=>{
-    console.log("Morning")
-}
-function fun(cb){
-    console.log("hello");
-}
-fun(greet);
